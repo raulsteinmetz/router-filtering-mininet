ce583042c45153ef3d3ff523db4d41d10427c560
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-12-08 15:51:43 -0300

    specification mandates to work with post instead of get

--- client-http.py
+++ client-http.py
@@ -2,11 +2,12 @@ import os
 
 def send_http_request():
     server_ip = '8.8.8.8'
-    port = '80'  # Assuming the HTTP server is running on port 80
+    port = '80'
     url = f'http://{server_ip}:{port}'
+    data = "sample_data=HelloWorld"
 
-    print(f"Sending HTTP request to {url}")
-    os.system(f'curl {url}')
+    print(f"Sending HTTP POST request to {url}")
+    os.system(f'curl -X POST -d "{data}" {url}')
 
 if __name__ == "__main__":
     send_http_request()
--- server-http.py
+++ server-http.py
@@ -1,14 +1,17 @@
 import http.server
 import socketserver
 
-PORT = 80  # Standard HTTP port
+PORT = 80
 
 class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) 
+        post_data = self.rfile.read(content_length)  
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"Hello, this is server1 responding to your request!")
+        response = f"Received POST request: {post_data.decode('utf-8')}"
+        self.wfile.write(response.encode('utf-8'))
 
 def run(server_class=http.server.HTTPServer, handler_class=SimpleHTTPRequestHandler):
     with server_class(("", PORT), handler_class) as httpd:
