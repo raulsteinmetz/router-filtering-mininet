d578305cca087a4e7e713e419bc80cb550ceec0e
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-12-09 08:33:34 -0300

    html inside http

--- client-http.py
+++ client-http.py
@@ -3,11 +3,10 @@ import os
 def send_http_request():
     server_ip = '8.8.8.8'
     port = '80'
-    url = f'http://{server_ip}:{port}'
-    data = "HelloWorld"
+    url = f'http://{server_ip}:{port}/index.html'  # Requesting the index.html file
 
-    print(f"Sending HTTP POST request to {url}")
-    os.system(f'curl -X POST -d "{data}" {url}')
+    print(f"Sending HTTP GET request to {url}")
+    os.system(f'curl {url}')
 
 if __name__ == "__main__":
     send_http_request()
--- server-http.py
+++ server-http.py
@@ -3,17 +3,17 @@ import socketserver
 
 PORT = 80
 
-class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):
-        content_length = int(self.headers['Content-Length']) 
-        post_data = self.rfile.read(content_length)  
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        response = f"Received POST request: {post_data.decode('utf-8')}"
+        response = f"Received POST request with data: {post_data.decode('utf-8')}"
         self.wfile.write(response.encode('utf-8'))
 
-def run(server_class=http.server.HTTPServer, handler_class=SimpleHTTPRequestHandler):
+def run(server_class=http.server.HTTPServer, handler_class=CustomHTTPRequestHandler):
     with server_class(("", PORT), handler_class) as httpd:
         print(f"Serving HTTP on port {PORT}")
         httpd.serve_forever()
