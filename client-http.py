c5750c69e8a312b80f277030aeb3bb66ed6e1a3b
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-12-13 08:45:03 -0300

    n times

--- client-http.py
+++ client-http.py
@@ -2,7 +2,7 @@ import os
 import argparse
 import time
 
-def send_http_request(long, mark_time, verbose):
+def send_http_request(long, mark_time, verbose, number_of_requests):
     server_ip = '8.8.8.8'
     port = '80'
 
@@ -17,22 +17,24 @@ def send_http_request(long, mark_time, verbose):
             f'http://{server_ip}:{port}/bw.html'
         ]
 
-    for url in urls:
-        if verbose:
-            print(f"Sending HTTP GET request to {url}")
+    for _ in range(number_of_requests):
+        for url in urls:
+            if verbose:
+                print(f"Sending HTTP GET request to {url}")
 
-        start_time = time.time()
-        os.system(f'curl --silent -o /dev/null {"-s" if not verbose else ""} {url}')
-        end_time = time.time()
+            start_time = time.time()
+            os.system(f'curl --silent -o /dev/null {"-s" if not verbose else ""} {url}')
+            end_time = time.time()
 
-        if mark_time:
-            print(f"Request to {url} completed in {end_time - start_time:.2f} seconds")
+            if mark_time:
+                print(f"Request to {url} completed in {end_time - start_time:.2f} seconds")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--long', action='store_true', help='Request URLs with lots of text')
     parser.add_argument('--mark_time', action='store_true', help='Print the time elapsed during requests')
     parser.add_argument('--verbose', action='store_true', help='Print curl command output')
+    parser.add_argument('--number_of_requests', type=int, default=1, help='Number of times to send requests')
     args = parser.parse_args()
 
-    send_http_request(args.long, args.mark_time, args.verbose)
+    send_http_request(args.long, args.mark_time, args.verbose, args.number_of_requests)
