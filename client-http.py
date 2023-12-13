264f5e7d515496237eb762b14e907af762dc448d
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-12-13 08:57:31 -0300

    plotting

--- client-http.py
+++ client-http.py
@@ -1,8 +1,9 @@
 import os
 import argparse
 import time
+import matplotlib.pyplot as plt
 
-def send_http_request(long, mark_time, verbose, number_of_requests):
+def send_http_request(long, mark_time, verbose, number_of_requests, plot):
     server_ip = '8.8.8.8'
     port = '80'
 
@@ -17,8 +18,8 @@ def send_http_request(long, mark_time, verbose, number_of_requests):
             f'http://{server_ip}:{port}/bw.html'
         ]
 
-    # Initialize a dictionary to store cumulative time for each URL
-    cumulative_time = {url: 0 for url in urls}
+    # Initialize a dictionary to store timings for each URL
+    timings = {url: [] for url in urls}
 
     for _ in range(number_of_requests):
         for url in urls:
@@ -30,17 +31,28 @@ def send_http_request(long, mark_time, verbose, number_of_requests):
             end_time = time.time()
             elapsed_time = end_time - start_time
 
-            # Add elapsed time to the cumulative time for this URL
-            cumulative_time[url] += elapsed_time
+            # Log the elapsed time for this URL
+            timings[url].append(elapsed_time)
 
             if mark_time:
                 print(f"Request to {url} completed in {elapsed_time:.2f} seconds")
 
-    # Calculate and print the average time for each URL
-    if mark_time:
-        for url in urls:
-            average_time = cumulative_time[url] / number_of_requests
-            print(f"Average time for {url}: {average_time:.2f} seconds")
+    if plot:
+        for url, times in timings.items():
+            plt.figure()
+            plt.plot(times, label='Response Time')
+            plt.xlabel('Request Number')
+            plt.ylabel('Time (seconds)')
+            plt.title(f'Timings for {url}')
+            plt.legend()
+            plt.grid(True)
+
+            # Ensure the analysis directory exists
+            if not os.path.exists('./analysis'):
+                os.makedirs('./analysis')
+
+            # Save the plot
+            plt.savefig(f'./analysis/timing_{url.split("/")[-1]}.png')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -48,6 +60,7 @@ if __name__ == "__main__":
     parser.add_argument('--mark_time', action='store_true', help='Print the time elapsed during requests')
     parser.add_argument('--verbose', action='store_true', help='Print curl command output')
     parser.add_argument('--number_of_requests', type=int, default=1, help='Number of times to send requests')
+    parser.add_argument('--plot', action='store_true', help='Plot the timings and save the plots')
     args = parser.parse_args()
 
-    send_http_request(args.long, args.mark_time, args.verbose, args.number_of_requests)
+    send_http_request(args.long, args.mark_time, args.verbose, args.number_of_requests, args.plot)
