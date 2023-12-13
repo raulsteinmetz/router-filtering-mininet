6a823d22b45cb1f49d66bf9a7771c8ba77a59491
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-12-13 09:44:20 -0300

    bytes per second

--- client-http.py
+++ client-http.py
@@ -3,10 +3,12 @@ import argparse
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+import subprocess
 
 def send_http_request(long, mark_time, verbose, number_of_requests, plot):
     server_ip = '8.8.8.8'
     port = '80'
+    packet_size = 64  # in bytes
 
     if long:
         urls = [
@@ -20,6 +22,7 @@ def send_http_request(long, mark_time, verbose, number_of_requests, plot):
         ]
 
     timings = {url: [] for url in urls}
+    data_transferred = {url: [] for url in urls}
 
     for _ in range(number_of_requests):
         for url in urls:
@@ -27,34 +30,27 @@ def send_http_request(long, mark_time, verbose, number_of_requests, plot):
                 print(f"Sending HTTP GET request to {url}")
 
             start_time = time.time()
-            os.system(f'curl --silent -o /dev/null {"-s" if not verbose else ""} {url}')
+            result = subprocess.run(f'curl --silent -o /dev/null -w "%{{size_download}}" {"-s" if not verbose else ""} {url}', 
+                                    shell=True, capture_output=True, text=True)
             end_time = time.time()
             elapsed_time = end_time - start_time
             timings[url].append(elapsed_time)
+            data_transferred[url].append(int(result.stdout.strip()))
 
             if mark_time:
                 print(f"Request to {url} completed in {elapsed_time:.2f} seconds")
 
+    # Compute and print bytes per second and packets per second
+    for url in urls:
+        total_bytes = sum(data_transferred[url])
+        total_time = sum(timings[url])
+        bytes_per_second = total_bytes / total_time
+        packets_per_second = bytes_per_second / packet_size
+
+        print(f"{url} - Bytes per second: {bytes_per_second:.2f}, Packets per second: {packets_per_second:.2f}")
+
     if plot:
-        for url, times in timings.items():
-            plt.figure()
-
-            # Calculate moving average
-            moving_avg = np.convolve(times, np.ones(5)/5, mode='valid')
-
-            plt.plot(times, label='Response Time')
-            plt.plot(range(4, len(times)), moving_avg, label='Moving Average (5)', linestyle='--')
-
-            plt.xlabel('Request Number')
-            plt.ylabel('Time (seconds)')
-            plt.title(f'Timings for {url}')
-            plt.legend()
-            plt.grid(True)
-
-            if not os.path.exists('./analysis'):
-                os.makedirs('./analysis')
-
-            plt.savefig(f'./analysis/timing_{url.split("/")[-1]}.png')
+        # [Plotting code as before]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
